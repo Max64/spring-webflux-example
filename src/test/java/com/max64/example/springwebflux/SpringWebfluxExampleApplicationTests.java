302e94325132b9e2c37ea5ab0e9cb55e035b6996
Max64 <you@example.com>
2020-05-17 19:28:05 +0300

    init project with spring initializr

---
+++ src/test/java/com/max64/example/springwebflux/SpringWebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max64.example.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
